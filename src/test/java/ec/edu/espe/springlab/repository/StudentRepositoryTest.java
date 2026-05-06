901dc0699bae7c5a474d8ccecd93f67abc3847bb
chestres23 <christianvasconez_2016@hotmail.com>
2026-05-06 16:42:48 -0500

    Create the test shouldSaveAndFindStundetByEmail

---
+++ src/test/java/ec/edu/espe/springlab/repository/StudentRepositoryTest.java
@@ -0,0 +1,37 @@
+package ec.edu.espe.springlab.repository;
+
+
+import ec.edu.espe.springlab.domain.Student;
+import org.hibernate.validator.internal.constraintvalidators.bv.AssertTrueValidator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class StudentRepositoryTest {
+    @Autowired
+    private StudentRepository studentRepository;
+    @Autowired
+    private AssertTrueValidator assertTrueValidator;
+
+    @Test
+    void shouldSaveAndFindStudentByEmail() {
+        Student student = new Student();
+        student.setFullName("Test User");
+        student.setEmail("test@hotmail.com");
+        student.setBirthdate(LocalDate.of(2003,8,23));
+        student.setActive(true);
+
+
+        studentRepository.save(student);
+
+        var result = studentRepository.findByEmail("test@hotmai.com");
+        assertThat(result).isPresent();
+        assertThat(result.get().getFullName()).isEqualTo("Test User");
+
+    }
+}
